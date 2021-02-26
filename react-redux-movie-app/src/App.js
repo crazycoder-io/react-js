8d7a137b0d830573479726fe2c06c1ddff161e18
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-02-27 00:33:06 +0300

    footer component

--- react-redux-movie-app/src/App.js
+++ react-redux-movie-app/src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
-import { Movies } from "./pages";
-import "./App.css";
-import {
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Image,
-  List,
-  Menu,
-  Segment,
-  Visibility,
-} from "semantic-ui-react";
+import { Container, Image, Menu, Visibility } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import { menuStyle, fixedMenuStyle } from "./helpers/styleHelper";
+import { Footer } from "./components";
+import { Movies } from "./pages";
+import "./App.css";
 
 function App() {
   const [state, setState] = React.useState({
@@ -86,73 +77,7 @@ function App() {
         obcaecati tempora exercitationem. Praesentium amet dolores, ducimus fuga
         quisquam rem sed. Eos?
       </Container>
-
-      <Segment
-        inverted
-        style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
-        vertical
-      >
-        <Container textAlign="center">
-          <Grid columns={4} divided stackable inverted>
-            <Grid.Row>
-              <Grid.Column>
-                <Header inverted as="h4" content="Group 1" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column>
-                <Header inverted as="h4" content="Group 2" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column>
-                <Header inverted as="h4" content="Group 3" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column>
-                <Header inverted as="h4" content="Footer Header" />
-                <p>
-                  Extra space for a call to action inside the footer that could
-                  help re-engage users.
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-          <Divider inverted section />
-          <Image
-            src="https://react.semantic-ui.com/logo.png"
-            centered
-            size="mini"
-          />
-          <List horizontal inverted divided link size="small">
-            <List.Item as="a" href="#">
-              Site Map
-            </List.Item>
-            <List.Item as="a" href="#">
-              Contact Us
-            </List.Item>
-            <List.Item as="a" href="#">
-              Terms and Conditions
-            </List.Item>
-            <List.Item as="a" href="#">
-              Privacy Policy
-            </List.Item>
-          </List>
-        </Container>
-      </Segment>
+      <Footer />
     </div>
   );
 }
