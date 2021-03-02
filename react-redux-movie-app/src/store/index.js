d77bb87e1f088c611a502153ff28885360d55c12
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-03-02 14:02:23 +0300

    implement redux logger

--- react-redux-movie-app/src/store/index.js
+++ react-redux-movie-app/src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducer";
 
 export const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(thunk, logger))
 );
