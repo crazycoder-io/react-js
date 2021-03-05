9c61a8cc7600de996b3e344abf96a253a29ca33f
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-03-06 01:25:24 +0300

    update nav exact

--- react-redux-movie-app/src/components/Header.js
+++ react-redux-movie-app/src/components/Header.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Container, Image, Menu, Visibility } from "semantic-ui-react";
 import { menuStyle, fixedMenuStyle } from "../helpers/styleHelper";
 
 const Header = () => {
   const [state, setState] = React.useState({
-    menuFixed: false,
-    overlayFixed: false,
+    menuFixed: null,
   });
 
   const stickTopMenu = () => setState({ menuFixed: true });
-  const unStickTopMenu = () => setState({ menuFixed: false });
+  const unStickTopMenu = () => setState({ menuFixed: null });
 
   return (
     <Visibility
@@ -24,11 +23,11 @@ const Header = () => {
         style={state.menuFixed ? fixedMenuStyle : menuStyle}
       >
         <Container text>
-          <Menu.Item>
+          <Menu.Item as={NavLink} to="/" exact>
             <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
+            <Menu.Item header>takilio</Menu.Item>
           </Menu.Item>
-          <Menu.Item header>takilio</Menu.Item>
-          <Menu.Item as={Link} to="/movies">
+          <Menu.Item as={NavLink} to="/movies">
             Movies
           </Menu.Item>
           <Menu.Item as="a">Add New</Menu.Item>
