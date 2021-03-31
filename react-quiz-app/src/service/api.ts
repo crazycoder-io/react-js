63939716da6ebf69918c84f0808a086bceeee1a2
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-03-31 22:02:13 +0300

    fetch questions from api

--- react-quiz-app/src/service/api.ts
+++ react-quiz-app/src/service/api.ts
@@ -1,1 +1,17 @@
-https://opentdb.com/api.php?amount=10&type=multiple
+// https://opentdb.com/api.php?amount=10&type=multiple
+
+export enum Difficulty {
+    EASY='easy',
+    MEDIUM='medium',
+    HARD='hard',
+};
+
+export const fetchQuizQuestion = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    try {
+        const data = await (await fetch(endpoint)).json();
+        console.log(data);
+    } catch (e) {
+        console.log(e);
+    }
+}; 
