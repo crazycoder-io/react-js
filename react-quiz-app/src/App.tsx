b20459302cb51b4b3ff3c22f6b6ffc9fc0edb8a2
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-03-31 22:42:14 +0300

    fetch questions
    implement questions to interface

--- react-quiz-app/src/App.tsx
+++ react-quiz-app/src/App.tsx
@@ -1,21 +1,42 @@
 import React, {useState} from 'react';
-import { fetchQuizQuestion, Difficulty } from './service/api';
+import { fetchQuizQuestion, Difficulty, QuestionState } from './service/api';
 // Components
 import {QuestionCard} from './components';
 
 const TOTAL_QUESTIONS = 10;
 
+type AnswerObject = {
+    question: string;
+    answer: string;
+    correct: boolean;
+    correctAnswer: string;
+};
+
 const App = () => {
     const [loading, setLoading] = useState(false);
-    const [questions, setQuestions] = useState([]);
+    const [questions, setQuestions] = useState<QuestionState[]>([]);
     const [number, setNumber] = useState(0);
-    const [userAnswers, setUserAnswers] = useState([]);
+    const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
     const [score, setScore] = useState(0);
     const [gameOver, setGameOver] = useState(true);
 
-    fetchQuizQuestion(TOTAL_QUESTIONS, Difficulty.EASY);
+    const startTrivia = async () => {
+        try {
+            setLoading(true);
+            setGameOver(false);
 
-    const startTrivia = async () => {};
+            const newQuestions = await fetchQuizQuestion(TOTAL_QUESTIONS, Difficulty.EASY);
+
+            setQuestions(newQuestions);
+            setScore(0);
+            setUserAnswers([]);
+            setNumber(0);
+            setLoading(false);
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
+    };
 
     const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
@@ -24,18 +45,25 @@ const App = () => {
     return (
         <div>
             <h1>React Quiz App</h1>
-            <button className="start" onClick={startTrivia}>Start</button>
-            <p className="score">Score: </p>
-            <p>Loading Questions...</p>
-            {/* <QuestionCard 
-                questionNumber={number + 1}
-                totalQuestions={TOTAL_QUESTIONS}
-                question={questions[number].question}
-                answers={questions[number].answers}
-                userAnswer={userAnswers ? userAnswers[number] : undefined}
-                callback={checkAnswer}
-            /> */}
-            <button className="next" onClick={nextQuestion}>Next Question</button>
+            {userAnswers.length === TOTAL_QUESTIONS || gameOver && (
+                <button className="start" onClick={startTrivia}>Start</button>
+            )}
+            {!gameOver && <p className="score">Score: </p>}
+            {loading && <p>Loading Questions...</p>}
+            {!gameOver && !loading && (
+                <QuestionCard
+                    key={number}
+                    questionNumber={number + 1}
+                    totalQuestions={TOTAL_QUESTIONS}
+                    question={questions[number].question}
+                    answers={questions[number].answers}
+                    userAnswer={userAnswers ? userAnswers[number] : undefined}
+                    callback={checkAnswer}
+                />
+            )}
+            {!gameOver && !loading && (
+                <button className="next" onClick={nextQuestion}>Next Question</button>
+            )}
         </div>
     );
 }
--- react-quiz-app/src/service/api.ts
+++ react-quiz-app/src/service/api.ts
@@ -1,4 +1,16 @@
 // https://opentdb.com/api.php?amount=10&type=multiple
+import { shuffleArray } from '../utils/utils';
+
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
     EASY='easy',
@@ -10,7 +22,12 @@ export const fetchQuizQuestion = async (amount: number, difficulty: Difficulty) 
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     try {
         const data = await (await fetch(endpoint)).json();
-        console.log(data);
+        return data.results.map((question: Question) => (
+            {
+                ...question,
+                answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+            }
+        ));
     } catch (e) {
         console.log(e);
     }
