baeebe813cffc4cfb2e943e29c6cee33c17fb893
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-04-17 10:14:50 +0300

    react-redux-movie-app
    dotenv install and use

--- react-redux-movie-app/src/config/env.js
+++ react-redux-movie-app/src/config/env.js
@@ -1,6 +1,2 @@
 // export const API_URI = "http://localhost:8080";
 export const API_URI = "https://nodejs-movieapi.herokuapp.com";
-export const API_USER = {
-  userName: "crazycoder",
-  password: "movieuser",
-};
